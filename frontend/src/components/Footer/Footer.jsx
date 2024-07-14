577132488c55da2bc1f1817f4a65650a10ce9776
misprachi023 <prachimishra23123@gmail.com>
2024-07-14 21:45:51 +0530

    update home page

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import './footer.css'
-import { Container, Row, Col, ListGroup, ListGroupItem } from 'reactstrap'
-import { Link } from 'react-router-dom'
-import logo from '../../assets/images/logo.png'
+import React from "react";
+import "./footer.css";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
 
 const quick_links = [
   {
@@ -33,19 +33,102 @@ const quick_links2 = [
   },
 ];
 
-
 const Footer = () => {
   return (
     <footer className="footer">
       <Container>
         <Row>
-          <Col lg="3" >
+          <Col lg="3">
+            <div className="logo">
+              <img src={logo} alt="logo" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            </div>
+            <div className="social__links d-flex align-items-center gap-4">
+              <span>
+                <Link to="#">
+                  <i class="ri-youtube-line"></i>
+                </Link>
+              </span>
+              <span>
+                <Link to="#">
+                  <i class="ri-github-fill"></i>
+                </Link>
+              </span>
+              <span>
+                <Link to="#">
+                  <i class="ri-facebook-circle-line"></i>
+                </Link>
+              </span>
+              <span>
+                <Link to="#">
+                  <i class="ri-instagram-line"></i>
+                </Link>
+              </span>
+            </div>
+          </Col>
 
+          <Col lg="3">
+            <h5 className="footer__link-title">Discover</h5>
+            <ListGroup className="footer__quick-links">
+              {quick_links.map((item, index) => (
+                <ListGroupItem key={index} className="ps-0 border-0">
+                  <Link to={item.path}>{item.display}</Link>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col lg="3">
+            <h5 className="footer__link-title">Quick Links</h5>
+            <ListGroup className="footer__quick-links">
+              {quick_links2.map((item, index) => (
+                <ListGroupItem key={index} className="ps-0 border-0">
+                  <Link to={item.path}>{item.display}</Link>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col lg="3">
+            <h5 className="footer__link-title">Contact</h5>
+            <ListGroup className="footer__quick-links">
+              <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
+                <h6 className="mb-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-map-pin-line"></i>Address:
+                  </span>
+                </h6>
+
+                <p className="mb-0">Kolar Road, Bhopal</p>
+              </ListGroupItem>
+              <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
+                <h6 className="mb-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-mail-line"></i>Email:
+                  </span>
+                </h6>
+
+                <p className="mb-0">prachimishra23123@gmail.com</p>
+              </ListGroupItem>
+              <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
+                <h6 className="mb-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-phone-line"></i>Phone:
+                  </span>
+                </h6>
+
+                <p className="mb-0">0123456789</p>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+
+          <Col lg="12" className="text-center pt-5">
+            <p className="copyright">
+              Copyright {new Date().getFullYear()}, design and developed by Prachi Mishra. All rights reserved
+            </p>
           </Col>
         </Row>
       </Container>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
