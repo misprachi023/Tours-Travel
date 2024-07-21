620c375bbdcfc20537a557fdc5fc2db1d4a3c160
misprachi023 <prachimishra23123@gmail.com>
2024-07-21 20:21:59 +0530

    fix code

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 const corsOptions={
-    origin:true,
+    origin:'http://127.0.0.1:5173',
     credentials:true
 }
 
